416b0c178ba48705cb651fa851811e23ccf65cfd
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-04-04 17:43:06 +0530

    Add files via upload

---
+++ Qn8.vue
@@ -0,0 +1,47 @@
+<template>
+
+<div>
+<p>Question8</p>
+<p @click="result()">p tag</p>
+
+
+<textarea id="sample"> </textarea>
+
+</div>
+
+</template>
+
+
+
+
+<script>
+
+export default {
+
+name: "QuesEight",
+
+
+data() {
+
+return {
+
+text: "",
+
+};
+
+},
+
+
+methods: {
+
+result() {
+
+this.text = document.write("" + document.getElementById("sample").value);
+
+},
+
+},
+
+};
+
+</script>
