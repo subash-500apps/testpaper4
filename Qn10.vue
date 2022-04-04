0972cbba5d2f2d4b756c0cf431a54ed5ae1c4483
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-04-04 15:58:14 +0530

    Add files via upload

---
+++ Qn10.vue
@@ -0,0 +1,38 @@
+<template>
+   <div class ="test">
+       <h1>This is Qn10</h1>
+       <input type="text" placeholder="firstname" vue-model="firstname">
+       <input type="text" placeholder="lastname" vue-model ="lastname">
+       <p>{{fullName('welcome')}}</p>
+     
+   </div>
+       </template>
+       <script>
+
+
+
+export default{
+
+   
+    name:'tableData',
+    
+    
+             computed:{
+     fullName(){
+      return message1 => {
+        return `${message1} ${this.firstName}  ${this.lastName}`
+      }
+    }
+  },
+
+  
+  data(){
+    return{
+      firstName: "",
+      lastName: ""
+    }
+  },
+  
+}
+
+  </script>
