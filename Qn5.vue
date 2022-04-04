fa1dcabcf7a0514bb7334ff72fa3e443f3ebb63b
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-04-04 16:27:42 +0530

    Add files via upload

---
+++ Qn5.vue
@@ -0,0 +1,42 @@
+<template>
+
+<div>
+<p>Fifth Ques</p>
+
+<button @click="errorhandling()">click</button>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+
+name:"fifthQues",
+
+methods: {
+
+errorhandling(){
+
+try{
+
+this.test=this.value*45;
+
+console.log("test",this.value);
+
+}
+
+catch{
+
+console.log("error handled");
+
+}
+
+}
+
+}
+
+}
+
+</script>
