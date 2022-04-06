8ca5ccf592f5171339d603ce806e260f7b410612
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-04-06 19:11:00 +0530

    Add files via upload

---
+++ errorhand.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div>
+
+
+
+<p>TenthQues</p>
+
+<button @click="errorhandling()">click</button>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+
+name:"tenthQues",
+
+methods: {
+
+errorhandling(){
+
+try{
+
+this.count=this.count*5;
+
+console.log("printing count",this.count);
+
+}
+
+catch{
+
+console.log("count has no initialisation");
+
+}
+
+}
+
+}
+
+}
+
+</script>
