74f4f996f1473e112557d5a9ce40f547b5bdc941
subash-500apps <102018284+subash-500apps@users.noreply.github.com>
2022-04-07 11:00:05 +0530

    Add files via upload

---
+++ refsparent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+  
+  <que4a :message="message" :name="name" ref="refg" :number="number" :mantra="mantra"></que4a>
+  <que4a :message="message" :name="name" ref="madan" :number="number" :Items="Items"></que4a>
+
+
+    hello world
+  <button @click="click()">submit</button>
+  
+
+
+  </div>
+</template>
+
+
+<script>
+import que4a from '../components/Refschild'
+export default {
+  name: 'HelloWorld',
+  //local declaration in que-3
+  components: {
+    
+    que4a,  
+  },
+  data(){
+    return{
+      message:"vue components",
+      name:"madan",
+    
+      Items:[
+      {
+          age:20,firstname:"sweet",lastname:"sugan"
+      },
+
+      ],
+      mantra:"mantra technologies"
+    }
+  },
+  methods:{
+    click(){
+      this.$refs.refg.fun();
+      this.$refs.madan.Items;
+      alert("avengers assemble")
+    }
+  }
+}
+</script>
